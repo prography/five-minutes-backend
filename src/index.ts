6bedc62d8a4f36c7f9f550205cfe5ec7c2e6eb67
Geusan Lim <dnay2k@gmail.com>
2019-04-12 03:32:20 +0900

    실행되는 포트 로그 변경

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ const HOST: string = process.env.HOST || 'localhost';
 const PORT: number = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, HOST, () => {
-  console.log('Server is listening on port 3000!');
+  console.log(`Server is listening on port ${PORT}!`);
 });
