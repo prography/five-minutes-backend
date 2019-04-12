d78b13a465f1bca26e79a611f583d595e7df695a
Geusan Lim <dnay2k@gmail.com>
2019-04-13 04:03:03 +0900

    데이터베이스 네이밍 전략 설정
    변수명은 camelCase, 데이터베이스는 snakeCase로 작성

---
+++ src/utils/DataBaseNamingStrategy.ts
@@ -0,0 +1,26 @@
+import { plural } from 'pluralize';
+import { DefaultNamingStrategy } from 'typeorm';
+import { snakeCase } from 'typeorm/util/StringUtils';
+
+export default class NamingStrategy extends DefaultNamingStrategy {
+  tableName(targetName: string, userSpecifiedName: string | undefined): string {
+    return plural(snakeCase(targetName));
+  }
+
+  relationName(propertyName: string): string {
+    return snakeCase(propertyName);
+  }
+
+  columnName(propertyName: string, customName: string) {
+    return snakeCase(customName || propertyName);
+  }
+
+  joinColumnName(relationName: string, referencedColumnName: string) {
+    return snakeCase(`${relationName}_${referencedColumnName}`);
+  }
+
+  joinTableColumnName(tableName: string, propertyName: string, columnName: string) {
+    return snakeCase(`${tableName}_${(columnName ? columnName : propertyName)}`);
+  }
+
+}
