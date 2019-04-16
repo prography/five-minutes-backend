e69a458fd5eaa36e6eed5d025610574a03f079bf
doopark <new0771@gmail.com>
2019-04-16 23:41:27 +0900

    question service-1

--- src/services/QuestionService.ts
+++ src/services/QuestionService.ts
@@ -1,36 +1,78 @@
+import { DeleteResult, FindOneOptions } from 'typeorm';
 import { Question } from '../models/Question';
+import { QuestionLike } from '../models/QuestionLike';
+import { QuestionTag } from '../models/QuestionTag';
+import { User } from '../models/User';
+import { QuestionLikeRepository } from '../repositories/QuestionLikeRepository';
 import { QuestionRepository } from '../repositories/QuestionRepository';
+import { QuestionTagRepository } from '../repositories/QuestionTagRepository';
+import { UserRepository } from '../repositories/UserRepository';
 
 export class QuestionService {
 
   private questionRepository: QuestionRepository;
-
+  private questionLikeRepository: QuestionLikeRepository;
+  private questiontagRepository: QuestionTagRepository;
+  private userRepository: UserRepository;
   constructor() {
     this.questionRepository = new QuestionRepository();
+    this.questionLikeRepository = new QuestionLikeRepository();
+    this.questiontagRepository = new QuestionTagRepository();
+    this.userRepository = new UserRepository();
   }
 
-  create() {}
+  create(question: Question): Promise<Question> {
+    const newQuestion = new Question();
+    newQuestion.subject = question.subject;
+    newQuestion.content = question.content;
+    newQuestion.code = question.code;
+    newQuestion.user = question.user;
+    newQuestion.likedUsers = question.likedUsers;
+    newQuestion.comments = question.comments;
+    newQuestion.tags = question.tags;
+    return this.questionRepository.create(newQuestion);
+  }
 
-  update() {}
+  update(id: number, question: Partial<Question>): Promise<Question> {
+    return this.questionRepository.update(id, question);
+  }
 
-  getQuestions() {}
+  delete(id: number): Promise<DeleteResult> {
+    return this.questionRepository.delete(id);
+  }
 
-  async getQuestion(id: number): Promise<Question | undefined> {
+  getQuestion(id: number): Promise<Question | undefined> {
     return this.questionRepository.findById(id);
   }
 
-  countLikes() {}
+  countLikes(id: number): Promise<QuestionLike | undefined> {
+    return this.questionLikeRepository.findById(id);
+  }
 
-  addTag() {}
+  addTag(questiontag: QuestionTag): Promise<QuestionTag | undefined> {
+    const newTag = new QuestionTag();
+    newTag.tag = questiontag.tag;
+    newTag.question = questiontag.question;
+    return this.questiontagRepository.create(newTag);
+  }
 
-  removeTag() {}
+  removeTag(id: number): Promise<DeleteResult> {
+    return this.questiontagRepository.delete(id);
+  }
 
-  delete() {}
+  likeQuestion(questionlike: QuestionLike): Promise<QuestionLike | undefined> {
+    const newquestionlike = new QuestionLike();
+    newquestionlike.user = questionlike.user;
+    newquestionlike.question = questionlike.question;
+    return this.questionLikeRepository.create(questionlike);
+  }
 
-  getLikedQuestions() {}
+  getLikedQuestions(likedquestions: FindOneOptions<User>): Promise<User | undefined> {
+  // findone 옵션 어떻게 주는지 모르겠음
+    return this.userRepository.findOne(likedquestions);
+  }
 
   getQuestionsByTags() {}
 
-  likeQuestion() {}
-
+  getQuestions() {}
 }
