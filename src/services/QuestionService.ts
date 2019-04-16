05284c1aeaa089d03fab2dbab5e426388930cc23
Geusan Lim <dnay2k@gmail.com>
2019-04-17 02:55:41 +0900

    QuestionService.ts 일부 수정

--- src/services/QuestionService.ts
+++ src/services/QuestionService.ts
@@ -1,35 +1,34 @@
-import { DeleteResult, FindOneOptions } from 'typeorm';
+import { DeleteResult } from 'typeorm';
 import { Question } from '../models/Question';
 import { QuestionLike } from '../models/QuestionLike';
 import { QuestionTag } from '../models/QuestionTag';
+import { Tag } from '../models/Tag';
 import { User } from '../models/User';
 import { QuestionLikeRepository } from '../repositories/QuestionLikeRepository';
 import { QuestionRepository } from '../repositories/QuestionRepository';
 import { QuestionTagRepository } from '../repositories/QuestionTagRepository';
-import { UserRepository } from '../repositories/UserRepository';
+import { TagRepository } from '../repositories/TagRepository';
 
 export class QuestionService {
 
   private questionRepository: QuestionRepository;
   private questionLikeRepository: QuestionLikeRepository;
-  private questiontagRepository: QuestionTagRepository;
-  private userRepository: UserRepository;
+  private questionTagRepository: QuestionTagRepository;
+  private tagRepository: TagRepository;
+
   constructor() {
     this.questionRepository = new QuestionRepository();
     this.questionLikeRepository = new QuestionLikeRepository();
-    this.questiontagRepository = new QuestionTagRepository();
-    this.userRepository = new UserRepository();
+    this.questionTagRepository = new QuestionTagRepository();
+    this.tagRepository = new TagRepository();
   }
 
-  create(question: Question): Promise<Question> {
+  create(user: User, subject: string, content: string, code: string): Promise<Question> {
     const newQuestion = new Question();
-    newQuestion.subject = question.subject;
-    newQuestion.content = question.content;
-    newQuestion.code = question.code;
-    newQuestion.user = question.user;
-    newQuestion.likedUsers = question.likedUsers;
-    newQuestion.comments = question.comments;
-    newQuestion.tags = question.tags;
+    newQuestion.subject = subject;
+    newQuestion.content = content;
+    newQuestion.code = code;
+    newQuestion.user = user;
     return this.questionRepository.create(newQuestion);
   }
 
@@ -45,31 +44,50 @@ export class QuestionService {
     return this.questionRepository.findById(id);
   }
 
-  countLikes(id: number): Promise<QuestionLike | undefined> {
-    return this.questionLikeRepository.findById(id);
+  async countLikes(id: number): Promise<number> {
+    const result = await this.questionLikeRepository.findWithCount({ where: { id } });
+    return result[1];
   }
 
-  addTag(questiontag: QuestionTag): Promise<QuestionTag | undefined> {
+  async addTag(name: string, question: Question): Promise<QuestionTag | undefined> {
+    let tag = await this.tagRepository.findOne({ where: { name } });
+    // 쓰려는 태그가 없는 경우
+    if (!tag) {
+      const newTag = new Tag();
+      newTag.name = name;
+      newTag.description = '아직 설명이 없습니다.';
+      tag = await this.tagRepository.create(newTag);
+    }
+    const isTagged = await this.questionTagRepository.findOne({ where: { name, question } });
+    // 이미 태그가 달려 있는 경우
+    if (!!isTagged) throw Error('TAGGED_ALREADY');
     const newTag = new QuestionTag();
-    newTag.tag = questiontag.tag;
-    newTag.question = questiontag.question;
-    return this.questiontagRepository.create(newTag);
+    newTag.tag = tag;
+    newTag.question = question;
+    return this.questionTagRepository.create(newTag);
   }
 
-  removeTag(id: number): Promise<DeleteResult> {
-    return this.questiontagRepository.delete(id);
+  async removeTag(name: string, question: Question): Promise<DeleteResult> {
+    const target = await this.questionTagRepository.findOne({ where: { name, question } });
+    if (!target) throw Error('DOES_NOT_TAGGED');
+    return this.questionTagRepository.delete(target.id);
   }
 
-  likeQuestion(questionlike: QuestionLike): Promise<QuestionLike | undefined> {
-    const newquestionlike = new QuestionLike();
-    newquestionlike.user = questionlike.user;
-    newquestionlike.question = questionlike.question;
-    return this.questionLikeRepository.create(questionlike);
+  async likeQuestion(user: User, question: Question): Promise<QuestionLike | DeleteResult |undefined> {
+    const target = await this.questionLikeRepository.findOne({ where: { user, question } });
+    if (!target) {
+      const newLikedQuestion = new QuestionLike();
+      newLikedQuestion.user = user;
+      newLikedQuestion.question = question;
+      return this.questionLikeRepository.create(newLikedQuestion);
+    }
+    return this.questionLikeRepository.delete(target.id);
   }
 
-  getLikedQuestions(likedquestions: FindOneOptions<User>): Promise<User | undefined> {
+  getLikedQuestions(user: User): Promise<[QuestionLike[], number]> {
   // findone 옵션 어떻게 주는지 모르겠음
-    return this.userRepository.findOne(likedquestions);
+  // https://typeorm.io/#/find-options 공부하자
+    return this.questionLikeRepository.findWithCount({ where: { user } });
   }
 
   getQuestionsByTags() {}
