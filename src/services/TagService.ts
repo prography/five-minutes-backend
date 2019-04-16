60a7fe290ea7ba5ba5f8c8c7d56be0aa4c3e2c6f
Geusan Lim <dnay2k@gmail.com>
2019-04-17 02:15:42 +0900

    TagService.ts 내용 일부 수정

--- src/services/TagService.ts
+++ src/services/TagService.ts
@@ -1,6 +1,5 @@
-import { DeleteResult } from 'typeorm';
+import { DeleteResult, Like } from 'typeorm';
 import { Tag } from '../models/Tag';
-
 import { TagRepository } from '../repositories/TagRepository';
 
 export class TagService {
@@ -14,10 +13,21 @@ export class TagService {
   create(tag: Tag): Promise<Tag> {
     const newTag = new Tag();
     newTag.name = tag.name;
-    newTag.description = tag.content;
-    newTag.taggedQuestions = [];
-    newTag.taggedUsers = [];
-    return this.TagRepository.create(newTag);
+    newTag.description = tag.description;
+    return this.tagRepository.create(newTag);
+  }
+
+  // Tag 검색
+  getByName(name: string, take: number): Promise<[Tag[], number]> {
+    return this.tagRepository.findWithCount({ take, where: { name: Like(name) } });
+  }
+
+  // Tag 수정
+  update(id: number, tag: Partial<Tag>): Promise<Tag> {
+    return this.tagRepository.update(id, {
+      name: tag.name,
+      description: tag.description,
+    });
   }
 
   delete(id: number): Promise<DeleteResult> {
