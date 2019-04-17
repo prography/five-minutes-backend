5ddfe457de7fd7afaa458f7ede7a65f669693677
Geusan Lim <dnay2k@gmail.com>
2019-04-18 02:46:40 +0900

    웹 어플리케이션 실행 함수 생성
    async 함수로 생성하여, 데이터베이스 연결과 어플리케이션 실행이 둘다 완벽하게 이루어 지도록 준비

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import app from './app';
+import * as database from './database';
 
 const HOST: string = process.env.HOST || 'localhost';
 const PORT: number = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server is listening on port ${PORT}!`);
-});
+async function startApplication() {
+  try {
+    // 데이터베이스 연결
+    await database.connect();
+    console.log(`database is connected successfully`);
+    // 어플리케이션 실행
+    await app.listen(PORT, HOST);
+    console.log(`Server is listening on port ${PORT}!`);
+  } catch (e) {
+    console.error(`Server has fatal error: ${e}`);
+  }
+}
+
+// 서버 실행
+startApplication();
