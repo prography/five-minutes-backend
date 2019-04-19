69f5cbc88dbf547621cc5f8771aac464c05bc365
Geusan Lim <dnay2k@gmail.com>
2019-04-20 00:30:08 +0900

    UserController 작성

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,39 +1,51 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseInterceptor } from 'routing-controllers';
+import { Body, Controller, Delete, Get, Param, Post, Put, QueryParam, UseInterceptor } from 'routing-controllers';
+import { UserCreateDto } from '../Dto/UserCreateDto';
 import { PaginationInterceptor } from '../interceptors/PaginationInterceptor';
 import { User } from '../models/User';
+import { UserService } from '../services/UserService';
 
 @Controller('/users')
 export class UserController {
 
   @Post('/')
-  create() {
-    return 'result';
+  create(@Body() user: UserCreateDto) {
+    return new UserService().create(
+      user.email,
+      user.nickname,
+      user.password,
+      user.githubUrl,
+    );
   }
 
   @Get('/')
   @UseInterceptor(PaginationInterceptor)
-  getUsers() {
+  async getUsers(
+    @QueryParam('page', { required: true }) page: number,
+    @QueryParam('perPage', { required: true }) perPage: number,
+  ) {
+    const [items, totalCount] = await new UserService().getUsers(perPage, (page - 1) * perPage);
     return {
-      items: [],
-      page: 1,
-      perPage: 10,
-      totalCount: 100,
-      count: 10,
+      items,
+      totalCount,
+      page,
+      perPage,
+      count: items.length,
     };
   }
 
   @Get('/:id')
   getUser(@Param('id') id: number) {
-    return { id };
+    return new UserService().getUserById(id);
   }
 
   @Put('/:id')
   updateUser(@Param('id') id: number, @Body() user: User) {
-    return { ...user, id };
+    return new UserService().update(id, user);
   }
 
   @Delete('/:id')
-  deleteUser(@Param('id') id: number) {
+  async deleteUser(@Param('id') id: number) {
+    await new UserService().delete(id);
     return `delete item number ${id}`;
   }
 
