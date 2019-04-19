71e5061d293740075454be84e26782ed5eb58886
Geusan Lim <dnay2k@gmail.com>
2019-04-20 03:32:08 +0900

    댓글 생성 로직 수정, 객체의 id값을 받은 후에 객체를 찾아서 수정

--- src/services/CommentService.ts
+++ src/services/CommentService.ts
@@ -1,21 +1,29 @@
 import { Comment } from '../models/Comment';
 import { CommentRepository } from '../repositories/CommentRepository';
+import { QuestionRepository } from '../repositories/QuestionRepository';
+import { UserRepository } from '../repositories/UserRepository';
 
 export class CommentService {
 
   private commentRepository: CommentRepository;
+  private userRespository: UserRepository;
+  private questionRepository: QuestionRepository;
 
   constructor() {
     this.commentRepository = new CommentRepository();
+    this.userRespository = new UserRepository();
+    this.questionRepository = new QuestionRepository();
   }
 
-  create(comment: Comment): Promise<Comment> {
+  async create(content: string, codeline: number, questionId: number, userId: number): Promise<Comment> {
+    const user = await this.userRespository.findById(userId);
+    const question = await this.questionRepository.findById(questionId);
+    if (!user || !question) throw Error('NO_USER OR NO QUESTION');
     const newComment = new Comment();
-    newComment.codeline = comment.codeline;
-    newComment.content = comment.content;
-    newComment.likedUsers = comment.likedUsers;
-    newComment.question = comment.question;
-    newComment.user = comment.user;
+    newComment.codeline = codeline;
+    newComment.content = content;
+    newComment.question = question;
+    newComment.user = user;
     return this.commentRepository.create(newComment);
   }
 
