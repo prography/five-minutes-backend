54869ecc4e9f6089b82684f29482ee0185003fde
Geusan Lim <dnay2k@gmail.com>
2019-04-19 23:57:55 +0900

    QuestionController 로직 구현

--- src/controllers/QuestionController.ts
+++ src/controllers/QuestionController.ts
@@ -1,20 +1,50 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseInterceptor } from 'routing-controllers';
+import { Body, Controller, Delete, Get, Param, Post, Put, QueryParam, UseInterceptor } from 'routing-controllers';
+import { QuestionCreateDto } from '../Dto/QuestionCreateDto';
 import { EntityInterceptor } from '../interceptors/EntityInterceptor';
 import { PaginationInterceptor } from '../interceptors/PaginationInterceptor';
 import { Question } from '../models/Question';
+import { User } from '../models/User';
+import { QuestionService } from '../services/QuestionService';
 
 @Controller('/questions')
 export class QuestionController  {
 
   @Post('/')
   @UseInterceptor(EntityInterceptor)
-  create() {
-    return 'result';
+  create(@Body() question: QuestionCreateDto) {
+    return new QuestionService().create(
+      question.user,
+      question.subject,
+      question.content,
+      question.code,
+    );
   }
 
   @Get('/')
   @UseInterceptor(PaginationInterceptor)
-  getQuestions() {
+  async getQuestions(
+    @QueryParam('page', { validate: true }) page: number,
+    @QueryParam('perPage', { validate: true }) perPage: number,
+  ) {
+    const [items, totalCount] = await new QuestionService().getQuestions(perPage, (page - 1) * perPage);
+    return {
+      items,
+      totalCount,
+      page,
+      perPage,
+      count: items.length,
+    };
+  }
+
+  @Get('/:id')
+  @UseInterceptor(EntityInterceptor)
+  getQuestion(@Param('id') id: number) {
+    return new QuestionService().getQuestion(id);
+  }
+
+  @Get('/:id/comments')
+  @UseInterceptor(PaginationInterceptor)
+  getQuestionComments(@Param('id') id: number) {
     return {
       items: [],
       page: 1,
@@ -24,33 +54,22 @@ export class QuestionController  {
     };
   }
 
-  @Get('/:id')
-  @UseInterceptor(EntityInterceptor)
-  getQuestion(@Param('id') id: number) {
-    return { id };
-  }
-
-  @Get('/:id/comments')
-  @UseInterceptor(EntityInterceptor)
-  getQuestionComments(@Param('id') id: number) {
-    return { id };
-  }
-
   @Put('/:id/like')
   @UseInterceptor(EntityInterceptor)
-  likeQuestion(@Param('id') id: number) {
-    return { id, like: true };
+  likeQuestion(@Param('id') id: number, @Body() body: { user: User }) {
+    return new QuestionService().likeQuestion(body.user, id);
   }
 
   @Put('/:id')
   @UseInterceptor(EntityInterceptor)
-  updateQuestion(@Param('id') id: number, @Body() question: Question) {
-    return { ...question, id };
+  updateQuestion(@Param('id') id: number, @Body() question: Partial<Question>) {
+    return new QuestionService().update(id, question);
   }
 
   @Delete('/:id')
   @UseInterceptor(EntityInterceptor)
-  deleteQuestion(@Param('id') id: number) {
+  async deleteQuestion(@Param('id') id: number) {
+    await new QuestionService().delete(id);
     return `delete item number ${id}`;
   }
 
