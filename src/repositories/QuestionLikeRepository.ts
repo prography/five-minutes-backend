ad0c7fd0b246c53303c8acea071cdadd6ed07d36
Geusan Lim <dnay2k@gmail.com>
2019-04-27 02:12:20 +0900

    관계 모델 Repo 제거

--- src/repositories/QuestionLikeRepository.ts
+++
@@ -1,8 +0,0 @@
-import { QuestionLike } from '../models/QuestionLike';
-import { BaseRepository } from './base/BaseRepository';
-
-export class QuestionLikeRepository extends BaseRepository<QuestionLike> {
-  constructor() {
-    super(QuestionLike);
-  }
-}
