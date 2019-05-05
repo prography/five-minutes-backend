c85df2fbf35515791daae14a959adf8e0b314195
Geusan Lim <dnay2k@gmail.com>
2019-05-06 02:24:39 +0900

    토큰 생성에 필요한 정보를 interface로 분리

--- src/utils/AuthHelper.ts
+++ src/utils/AuthHelper.ts
@@ -1,17 +1,22 @@
 import crypto from 'crypto';
 import jsonwebtoken from 'jsonwebtoken';
 
+export interface AuthModel {
+  email: string;
+  rank: string;
+}
+
 export class AuthHelper {
 
-  static generate(info: { email: string, rank: string }) {
+  static generate(info: AuthModel) {
     const privateKey: string = process.env.JWT_SECRET || '';
     return jsonwebtoken.sign(info, privateKey, {
       expiresIn: '30d',
     });
   }
 
-  static extract(token: string) {
-    return jsonwebtoken.decode(token);
+  static extract(token: string): AuthModel | null {
+    return <AuthModel | null>jsonwebtoken.decode(token);
   }
 
   static hash(rawText: string) {
