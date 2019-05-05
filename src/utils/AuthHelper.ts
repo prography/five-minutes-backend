429979d9975e8527fc7c5042c81004ee3eec85d4
Geusan Lim <dnay2k@gmail.com>
2019-05-06 02:14:49 +0900

    비밀번호 암호화 로직 추가

--- src/utils/AuthHelper.ts
+++ src/utils/AuthHelper.ts
@@ -1,12 +1,20 @@
+import crypto from 'crypto';
 import jsonwebtoken from 'jsonwebtoken';
-import { Codec } from './Codec';
 
 export class AuthHelper {
+
   static generate(email: string, password: string) {
     const privateKey: string = process.env.JWT_SECRET || '';
-    return jsonwebtoken.sign({ email, password: Codec.hash(password) }, privateKey);
+    return jsonwebtoken.sign({ email, password: AuthHelper.hash(password) }, privateKey);
   }
+
   static extract(token: string) {
     return jsonwebtoken.decode(token);
+  }
+
+  static hash(rawText: string) {
+    const hasher = crypto.createHash('RSA-SHA256');
+    hasher.update(rawText);
+    return hasher.digest('hex');
   }
 }
