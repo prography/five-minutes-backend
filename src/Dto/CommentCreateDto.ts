063499c79f41217be0ef183110d97b133ffb633f
kang-heewon <itphantom@icloud.com>
2019-05-18 16:29:35 +0900

    Comment Dto

--- src/Dto/CommentCreateDto.ts
+++ src/Dto/CommentCreateDto.ts
@@ -4,6 +4,12 @@ export class CommentCreateDto {
   @IsString()
   @IsNotEmpty()
   content!: string;
+  @IsString()
+  @IsNotEmpty()
+  type!: string;
+  @IsString()
+  @IsNotEmpty()
+  status!: string;
   @IsNumber()
   @IsNotEmpty()
   codeline!: number;
