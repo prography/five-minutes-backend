72676ef27706c51b6c24d2665d6da6212b998908
Geusan Lim <dnay2k@gmail.com>
2019-05-23 09:32:52 +0900

    CommentUpdateDto 내용 변경
    수정에 필요한 데이터는 codeline과 content 뿐

--- src/Dto/CommentUpdateDto.ts
+++ src/Dto/CommentUpdateDto.ts
@@ -1,13 +1,9 @@
-import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CommentUpdateDto {
   @IsString()
   @IsNotEmpty()
   content!: string;
-  @IsEnum('string')
-  type!: string;
-  @IsEnum('string')
-  status!: string;
   @IsNumber()
   @IsNotEmpty()
   codeline!: number;
