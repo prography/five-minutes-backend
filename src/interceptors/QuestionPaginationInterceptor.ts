aea95434134f140cb5426c14c69ac1206907af85
Geusan Lim <dnay2k@gmail.com>
2019-06-08 16:05:54 +0900

    질문 목록 호출시 content는 텍스트만 나오도록 수정

--- src/interceptors/QuestionPaginationInterceptor.ts
+++ src/interceptors/QuestionPaginationInterceptor.ts
@@ -4,10 +4,10 @@ import { MarkdownCompiler } from '../utils/MarkdownCompiler';
 
 export class QuestionPaginationInterceptor implements InterceptorInterface {
 
-  intercept(_: Action, result: { items: (Partial<Question> & { summary?: string })[] }) {
+  intercept(_: Action, result: { items: Partial<Question>[] }) {
     result.items = result.items.map(item => ({
       ...item,
-      summary: MarkdownCompiler.compile(item.content || '', { length: 80 }),
+      content: MarkdownCompiler.compile(item.content || '', { length: 80 }),
     }));
     return {
       result,
