d4ff435f9c9e55d75bbf848f0075387b0d9a8c81
geusan <dnay2k@gmail.com>
2019-07-25 02:30:31 +0900

    테스트용 코드 추가

---
+++ src/utils/index.spec.ts
@@ -0,0 +1,30 @@
+// import { ObjectManager } from './ObjectManager';
+import fs from 'fs';
+import { Executer } from './Executer';
+import { ExecutableFile } from './Executer/Executer';
+
+// const a = {
+//   a: 1,
+//   b: 2,
+//   c: 13,
+//   d: [
+//     { a: 1, b: 10 },
+//     { a: 1, b: 10 },
+//     { a: 1, b: 10 },
+//     { a: 1, b: 10 },
+//   ],
+//   e: {
+//     a: 13,
+//     b: 12,
+//     c: new Date(),
+//   },
+// };
+
+// const deleteB = ObjectManager.deleteValuesByKeys(a, ['b']);
+
+// console.log(deleteB);
+
+const executer = new Executer();
+const text = fs.readFileSync(`${__dirname}/test.java`, { encoding: 'utf8' });
+const output = executer.execute(text, ExecutableFile.JAVA);
+console.log(output);
